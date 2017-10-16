e16689efca3a573f32cfc474e0f096ba63ec798d
Shreya Shah <shreyashah@Shreyas-MacBook-Pro.local>
2017-10-16 06:17:24 -0400

    Java Script

---
+++ labs/js-1/assets/js/app.js
@@ -0,0 +1,28 @@
+
+var cash = 15000;
+
+update_wallet();
+
+$('.item').click(function() {
+	var item =$(this) .data('item');
+	var amount =$(this) .data('amount');
+
+	charge_card(amount, item);
+
+});
+
+function charge_card(amount, item) {
+
+	if (cash >= amount) {
+	cash = cash - amount; 
+	update_wallet();
+	$('.closet-items').append(item +', ');
+} else { 
+	alert('not enough money.');
+	
+}
+}
+
+function update_wallet() {
+	$('.cash').html(cash);
+}
